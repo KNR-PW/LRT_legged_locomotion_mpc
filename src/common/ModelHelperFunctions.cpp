14b01f564df7fff6fa933559b00c4c15aadf4c29
Bartek <bartlomiejk@vp.pl>
2025-08-04 18:52:04 +0200

    Removed ocs2 namespace from src

--- src/common/ModelHelperFunctions.cpp
+++ src/common/ModelHelperFunctions.cpp
@@ -4,13 +4,13 @@ namespace floating_base_model
 {
   namespace model_helper_function
   {
-
+    using namespace ocs2;
     /******************************************************************************************************/
     /******************************************************************************************************/
     /******************************************************************************************************/
     template <typename SCALAR_T>
     Eigen::Matrix<SCALAR_T, Eigen::Dynamic, 1> computeApproxActuatedJointGeneralizedTorques(
-      ocs2::PinocchioInterfaceTpl<SCALAR_T>& interface,
+      PinocchioInterfaceTpl<SCALAR_T>& interface,
       const floating_base_model::FloatingBaseModelInfoTpl<SCALAR_T>& info,
       const Eigen::Matrix<SCALAR_T, Eigen::Dynamic, 1>& q,
       const pinocchio::container::aligned_vector<pinocchio::ForceTpl<SCALAR_T, 0>>& fext)
@@ -27,16 +27,16 @@ namespace floating_base_model
     /******************************************************************************************************/
     // Explicit template instantiation
 
-    template Eigen::Matrix<ocs2::scalar_t, Eigen::Dynamic, 1> computeApproxActuatedJointGeneralizedTorques(
-      ocs2::PinocchioInterfaceTpl<ocs2::scalar_t>& interface,
-      const floating_base_model::FloatingBaseModelInfoTpl<ocs2::scalar_t>& info,
-      const Eigen::Matrix<ocs2::scalar_t, Eigen::Dynamic, 1>& q,
-      const pinocchio::container::aligned_vector<pinocchio::ForceTpl<ocs2::scalar_t, 0>>& fext);
+    template Eigen::Matrix<scalar_t, Eigen::Dynamic, 1> computeApproxActuatedJointGeneralizedTorques(
+      PinocchioInterfaceTpl<scalar_t>& interface,
+      const floating_base_model::FloatingBaseModelInfoTpl<scalar_t>& info,
+      const Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>& q,
+      const pinocchio::container::aligned_vector<pinocchio::ForceTpl<scalar_t, 0>>& fext);
 
-    template Eigen::Matrix<ocs2::ad_scalar_t, Eigen::Dynamic, 1> computeApproxActuatedJointGeneralizedTorques(
-      ocs2::PinocchioInterfaceTpl<ocs2::ad_scalar_t>& interface,
-      const floating_base_model::FloatingBaseModelInfoTpl<ocs2::ad_scalar_t>& info,
-      const Eigen::Matrix<ocs2::ad_scalar_t, Eigen::Dynamic, 1>& q,
-      const pinocchio::container::aligned_vector<pinocchio::ForceTpl<ocs2::ad_scalar_t, 0>>& fext);
+    template Eigen::Matrix<ad_scalar_t, Eigen::Dynamic, 1> computeApproxActuatedJointGeneralizedTorques(
+      PinocchioInterfaceTpl<ad_scalar_t>& interface,
+      const floating_base_model::FloatingBaseModelInfoTpl<ad_scalar_t>& info,
+      const Eigen::Matrix<ad_scalar_t, Eigen::Dynamic, 1>& q,
+      const pinocchio::container::aligned_vector<pinocchio::ForceTpl<ad_scalar_t, 0>>& fext);
   }
 }
