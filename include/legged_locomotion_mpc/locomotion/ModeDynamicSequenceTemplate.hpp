92f2da2fac77d8212e27e8c2aa5b3f21eb4f245c
Bartek <bartlomiejk@vp.pl>
2025-08-14 19:59:20 +0200

    Implemented mode sequence template generator for dynamic parameters

---
+++ include/legged_locomotion_mpc/locomotion/ModeDynamicSequenceTemplate.hpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2025, Koło Naukowe Robotyków
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Authors: Bartłomiej Krajewski (https://github.com/BartlomiejK2)
+ */
+
+ // Copyright (c) 2025, Koło Naukowe Robotyków
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Authors: Bartłomiej Krajewski (https://github.com/BartlomiejK2)
+ */
+
+
+#ifndef __MODE_DYNAMIC_SEQUENCE_TEMPLATE_LEGGED_LOCOMOTION_MPC__
+#define __MODE_DYNAMIC_SEQUENCE_TEMPLATE_LEGGED_LOCOMOTION_MPC__
+
+#include <ocs2_legged_robot/gait/ModeSequenceTemplate.h>
+
+#include <legged_locomotion_mpc/common/Types.hpp>
+#include <legged_locomotion_mpc/locomotion/GaitParameters.hpp>
+
+namespace legged_locomotion_mpc
+{
+  namespace locomotion
+  {
+    ocs2::legged_robot::ModeSequenceTemplate getDynamicModeSequenceTemplate(
+      ocs2::scalar_t currentPhase,
+      ocs2::scalar_t timeHorizon,
+      const GaitStaticParameters& staticParams,
+      const GaitDynamicParameters& dynamicParams);
+  }; // namespace locomotion
+}; // namespace legged_locomotion_mpc
+
+
+#endif
---
+++ src/locomotion/ModeDynamicSequenceTemplate.cpp
@@ -0,0 +1,170 @@
+
+#include <legged_locomotion_mpc/locomotion/ModeDynamicSequenceTemplate.hpp>
+
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+#include <set>
+
+#include <legged_locomotion_mpc/locomotion/ModeCommon.hpp>
+
+namespace legged_locomotion_mpc
+{
+  namespace locomotion
+  {
+    using namespace ocs2;
+    using namespace ocs2::legged_robot;
+
+    double normalizeState(double value)
+    {
+      return value - std::floor(value);
+    }
+
+    double getTimeToNextMode(double currentState, double swingRatio, double gaitPeriod)
+    {
+      double timeToNextMode = 0;
+      if(currentState < swingRatio)
+      {
+        timeToNextMode = (swingRatio - currentState) * gaitPeriod;
+      }
+      else
+      {
+        timeToNextMode = (1 - currentState) * gaitPeriod;
+      }
+      return timeToNextMode;
+    }
+
+    // std::unordered_map<scalar_t, std::set<size_t>> getDuplicateIndexes(
+    //   const std::vector<scalar_t>& vector)
+    // {
+    //   std::unordered_map<scalar_t, std::set<size_t>> duplicates;
+    //   for(int i = 0; i < vector.size(); ++i)
+    //   {
+    //     for(int j = i + 1; j < vector.size(); ++j)
+    //     {
+    //       if(vector[i] == vector[j])
+    //       {
+    //         auto duplicateIterator = duplicates.find(vector[i]);
+    //         if(duplicateIterator == duplicates.end())
+    //         {
+    //           duplicates.emplace(vector[i], {i, j});
+    //         }
+    //         else
+    //         {
+    //           duplicateIterator->second.insert(j);
+    //         }
+    //       }
+    //     }
+    //   }
+    //   return duplicates;
+    // }
+
+     ModeSequenceTemplate getDynamicModeSequenceTemplate(
+      scalar_t currentPhase,
+      scalar_t timeHorizon,
+      const GaitStaticParameters& staticParams,
+      const GaitDynamicParameters& dynamicParams)
+    {
+      const auto& phaseOffsets = dynamicParams.phaseOffsets;
+      const scalar_t swingRatio = dynamicParams.swingRatio;
+      const scalar_t gaitPeriod = 1 / dynamicParams.steppingFrequency;
+
+      contact_flags_t currentMode;
+      std::vector<scalar_t> currentContactStates(staticParams.endEffectorNumber);
+
+      currentContactStates[0] = normalizeState(currentPhase);
+      currentMode[0] = normalizeState(currentPhase) >= swingRatio;
+      for(int i = 1; i < staticParams.endEffectorNumber; ++i)
+      {
+        currentContactStates[i] = normalizeState(currentPhase + phaseOffsets[i - 1]);
+        currentMode[i] = currentContactStates[i] >= swingRatio;
+      }
+
+      std::vector<scalar_t> switchingTimes;
+      switchingTimes.push_back(0.0);
+
+      std::vector<size_t> modeSequence;
+      modeSequence.push_back(currentMode.to_ulong());
+
+      /* If frequency is really low, just stand in place with current mode */
+      if(frequency < Definitions::MIN_STEPPING_FREQUENCY)
+      {
+        switchingTimes.push_back(timeHorizon);
+        return ModeSequenceTemplate(switchingTimes, modeSequence);
+      }
+
+      if(frequency > Definitions::MAX_STEPPING_FREQUENCY)
+      {
+        frequency = Definitions::MAX_STEPPING_FREQUENCY;
+      }
+
+      // /* Check if any offset phases are not the same */
+      // const auto duplicates = getDuplicateIndexes(phaseOffsets);
+      // bool areDuplicates = !duplicates.empty();
+
+      // if(areDuplicates)
+      // {
+        
+      // }
+
+      /* Min queue that gets earliest change */
+      using timeIndexQueue = std::priority_queue<std::pair<scalar_t, size_t>,
+        std::vector<std::pair<scalar_t, size_t>>,
+        std::greater<std::pair<scalar_t, size_t>>>;
+
+      timeIndexQueue timeEndEffectorIndexQueue;
+
+      for(size_t i = 0; i < staticParams.endEffectorNumber; ++i)
+      {
+        const scalar_t timeToNextMode = getTimeToNextMode(currentContactStates[i], swingRatio, gaitPeriod);
+        timeEndEffectorIndexQueue.push({timeToNextMode, i});
+      }
+
+      const scalar_t timeStance = (1 - swingRatio) * gaitPeriod;
+      const scalar_t timeSwing = gaitPeriod - timeStance;
+
+      scalar_t time = 0;
+
+      while(time < timeHorizon)
+      {
+        const size_t currentModeNum = modeSequence.back();
+        scalar_t nextTime;
+        size_t nextIndex;
+        std::tie(nextTime, nextIndex) = timeEndEffectorIndexQueue.top();
+        timeEndEffectorIndexQueue.pop();
+        currentMode[nextIndex] = !currentMode[nextIndex];
+
+        if(currentMode[nextIndex] == 0) // swing time
+        {
+          timeEndEffectorIndexQueue.push({nextTime + timeSwing, nextIndex});
+        }
+        else // stance time
+        {
+          timeEndEffectorIndexQueue.push({nextTime + timeStance, nextIndex});
+        }
+
+        while(nextTime == timeEndEffectorIndexQueue.top().first)
+        {
+          /* Found end effectors that have same offsets */
+          size_t anothertIndex = timeEndEffectorIndexQueue.top().second;
+          timeEndEffectorIndexQueue.pop();
+          currentMode[anothertIndex] = !currentMode[anothertIndex];
+
+          if(currentMode[anothertIndex] == 0) // swing time
+          {
+            timeEndEffectorIndexQueue.push({nextTime + timeSwing, anothertIndex});
+          }
+          else // stance time
+          {
+            timeEndEffectorIndexQueue.push({nextTime + timeStance, anothertIndex});
+          }
+
+          modeSequence.push_back(currentMode.to_ulong());
+          switchingTimes.push_back(nextTime);
+          time += nextTime;
+        }
+      }
+      return ModeSequenceTemplate(switchingTimes, modeSequence);
+    }
+  }
+}
