608f1dc0b2c7b6862005d0aded675b8309803721
Bartek <bartlomiejk@vp.pl>
2025-08-18 19:02:07 +0200

    Implemented controller for dynamic gait phase

---
+++ include/legged_locomotion_mpc/locomotion/GaitDynamicPhaseController.hpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2025, Koło Naukowe Robotyków
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Authors: Bartłomiej Krajewski (https://github.com/BartlomiejK2)
+ */
+
+
+#ifndef __GAIT_DYNAMIC_PHASE_LEGGED_LOCOMOTION_MPC__
+#define __GAIT_DYNAMIC_PHASE_LEGGED_LOCOMOTION_MPC__
+
+#include <legged_locomotion_mpc/common/Types.hpp>
+#include <legged_locomotion_mpc/locomotion/GaitParameters.hpp>
+
+namespace legged_locomotion_mpc
+{
+  namespace locomotion
+  {
+    class GaitDynamicPhaseController
+    {
+      public:
+        GaitDynamicPhaseController(ocs2::scalar_t initPhase, ocs2::scalar_t initTime,
+          const GaitStaticParameters& initStaticParams,
+          const GaitDynamicParameters& initDynamicParams);
+
+        /**
+         * Get phase values at given query time
+         */
+        std::vector<ocs2::scalar_t> getPhasesAtTime(ocs2::scalar_t time);
+
+        /**
+         * Get contact flags at given query time
+         */
+        contact_flags_t getContactFlagsAtTime(ocs2::scalar_t time);
+
+        /**
+         * Remove event times, frequencies and offsets before query time
+         */
+        void remove(ocs2::scalar_t time);
+
+        /**
+         * Add new frequency and offsets, starting from query time
+         */
+        void update(ocs2::scalar_t newTime, const GaitDynamicParameters& newDynamicParams);
+      private:
+
+        GaitStaticParameters staticParams_;
+
+        /**
+         * 
+         *  Frequencies are counted as follows:
+         *          ------ | ------ | ------ | ...  ------ | ------ | ------
+         *  time:         t[0]     t[1]     t[2]       t[n-1]     t[n]
+         *  frequency:        f[0]     f[1]     f[2] ...    f[n-1]     f[n]
+         *  offsets:        o[0]][:]  o[1]][:] o[2]][:]   o[n-1]][:]  o[n]][:]
+         *  swing ratio:      s[0]     s[1]     s[2]        s[n-1]     s[n]
+         *
+         * Where t[0] should have same value as ModeSchedule.eventTimes[0]!
+         */
+        std::vector<ocs2::scalar_t> eventTimes_;
+        std::vector<GaitDynamicParameters> dynamicParamsVec_;
+        
+        /**
+         * Phase value at time t[0] 
+         */
+        ocs2::scalar_t currentPhase_;
+    };
+  }; // namespace locomotion
+}; // namespace legged_locomotion_mpc
+
+#endif
---
+++ src/locomotion/GaitDynamicPhaseController.cpp
@@ -0,0 +1,117 @@
+#include <legged_locomotion_mpc/locomotion/GaitDynamicPhaseController.hpp>
+
+#include <legged_locomotion_mpc/locomotion/GaitCommon.hpp>
+
+#include <algorithm>
+
+namespace legged_locomotion_mpc
+{
+  namespace locomotion
+  {
+    using namespace ocs2;
+
+    GaitDynamicPhaseController::GaitDynamicPhaseController(ocs2::scalar_t initPhase,
+      ocs2::scalar_t initTime,
+      const GaitStaticParameters& initStaticParams,
+      const GaitDynamicParameters& initDynamicParams)
+        : currentPhase_(initPhase), staticParams_(initStaticParams)
+    {
+      eventTimes_.push_back(initTime);
+      dynamicParamsVec_.push_back(initDynamicParams);
+    }
+
+    std::vector<ocs2::scalar_t> GaitDynamicPhaseController::getPhasesAtTime(scalar_t time)
+    {
+      assert(time > eventTimes_.front());
+
+      std::vector<scalar_t> returnPhases(staticParams_.endEffectorNumber);
+      scalar_t returnPhase = currentPhase_;
+
+      // Find index of time larger that query time
+      const size_t index = std::lower_bound(eventTimes_.begin(), eventTimes_.end(),
+       time) - eventTimes_.begin();
+      
+      // Loop through fixed times
+      for(int i = 1; i < index - 1; ++i)
+      {
+        returnPhase += (eventTimes_[i] - eventTimes_[i - 1]) * dynamicParamsVec_[i - 1].steppingFrequency;
+      }
+      // Add last time
+      returnPhase += (time - eventTimes_[index - 1]) * dynamicParamsVec_[index - 1].steppingFrequency;
+
+      returnPhases[0] = normalizePhase(returnPhase);
+      const auto& offsets = dynamicParamsVec_[index - 1].phaseOffsets;
+      for(int i = 1; i < staticParams_.endEffectorNumber; ++i)
+      {
+        returnPhases[i] = normalizePhase(returnPhase + offsets[i]);
+      }
+
+      return returnPhases;
+    }
+
+    contact_flags_t GaitDynamicPhaseController::getContactFlagsAtTime(scalar_t time)
+    {
+      assert(time > eventTimes_.front());
+
+      contact_flags_t returnFlags(staticParams_.endEffectorNumber);
+      scalar_t returnPhase = currentPhase_;
+
+      // Find index of time larger that query time
+      const size_t index = std::lower_bound(eventTimes_.begin(), eventTimes_.end(),
+       time) - eventTimes_.begin();
+      
+      // Loop through fixed times
+      for(int i = 1; i < index - 1; ++i)
+      {
+        returnPhase += (eventTimes_[i] - eventTimes_[i - 1]) * dynamicParamsVec_[i - 1].steppingFrequency;
+      }
+      // Add last time
+      returnPhase += (time - eventTimes_[index - 1]) * dynamicParamsVec_[index - 1].steppingFrequency;
+
+      const auto& offsets = dynamicParamsVec_[index - 1].phaseOffsets;
+      const scalar_t swingRatio = dynamicParamsVec_[index - 1].swingRatio;
+
+      returnFlags[0] = normalizePhase(returnPhase) >= swingRatio? 1 : 0;
+      for(int i = 1; i < staticParams_.endEffectorNumber; ++i)
+      {
+        returnFlags[i] = normalizePhase(returnPhase + offsets[i]) >= swingRatio? 1 : 0;
+      }
+
+      return returnFlags;
+    }
+
+    void GaitDynamicPhaseController::remove(scalar_t time)
+    {
+      // Find index of time larger that query time
+      const size_t index = std::lower_bound(eventTimes_.begin(), eventTimes_.end(),
+       time) - eventTimes_.begin();
+
+      if(index == 0) return;
+
+      // Update current phase, so that it matches value in query time
+      // Loop through fixed times
+      for(int i = 1; i < index - 1; ++i)
+      {
+        currentPhase_ += (eventTimes_[i] - eventTimes_[i - 1]) * dynamicParamsVec_[i - 1].steppingFrequency;
+      }
+      // Add last time
+      currentPhase_ += (time - eventTimes_[index - 1]) * dynamicParamsVec_[index - 1].steppingFrequency;
+
+      // delete the old time and frequency values to last time smaller than query time
+      eventTimes_.erase(eventTimes_.begin(), eventTimes_.begin() + index - 1);
+      dynamicParamsVec_.erase(frequencies_.begin(), frequencies_.begin() + index - 1);
+
+      // Change last time smaller than query time to query time
+      eventTimes_.front() = time;
+    }
+
+    void GaitDynamicPhaseController::update(scalar_t newTime, 
+      const GaitDynamicParameters& newDynamicParams)
+    {
+      assert(newTime > eventTimes_.back());
+
+      eventTimes_.push_back(newTime);
+      dynamicParamsVec_.push_back(newDynamicParams);
+    }
+  }
+}
