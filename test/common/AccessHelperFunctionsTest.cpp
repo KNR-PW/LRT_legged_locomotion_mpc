834d4b0fec5222561c502086c59d66bbabb4ae06
Bartek <bartlomiejk@vp.pl>
2025-10-30 15:46:49 +0100

    Name change to vector6_t

--- test/common/AccessHelperFunctionsTest.cpp
+++ test/common/AccessHelperFunctionsTest.cpp
@@ -19,22 +19,22 @@ TEST(AccessHelperFunctions, RobotStateConst)
 
   // READ ACCESS
 
-  const Eigen::Vector<double, 6> baseVelocity = getBaseVelocity(state, info);
+  const vector6_t baseVelocity = getBaseVelocity(state, info);
   const Eigen::Vector<double, 3> baseLinearVelocity = getBaseLinearVelocity(state, info);
   const Eigen::Vector<double, 3> baseAngularVelocity = getBaseAngularVelocity(state, info);
 
   ASSERT_TRUE(baseLinearVelocity== Eigen::Vector3d(0.0, 1.0, 2.0));
   ASSERT_TRUE(baseAngularVelocity == Eigen::Vector3d(3.0, 4.0, 5.0));
-  const Eigen::Vector<double, 6> baseVelocityTrue = Eigen::Vector<double, 6>(0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
+  const vector6_t baseVelocityTrue = vector6_t(0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
   ASSERT_TRUE(baseVelocity == baseVelocityTrue);
   
-  const Eigen::Vector<double, 6> basePose = getBasePose(state, info);
+  const vector6_t basePose = getBasePose(state, info);
   const Eigen::Vector<double, 3> basePosition = getBasePosition(state, info);
   const Eigen::Vector<double, 3> baseOrientation = getBaseOrientationZyx(state, info);
 
   ASSERT_TRUE(basePosition == Eigen::Vector3d(6.0, 7.0, 8.0));
   ASSERT_TRUE(baseOrientation == Eigen::Vector3d(9.0, 10.0, 11.0));
-  const Eigen::Vector<double, 6> basePoseTrue = Eigen::Vector<double, 6>(6.0, 7.0, 8.0, 9.0, 10.0, 11.0);
+  const vector6_t basePoseTrue = vector6_t(6.0, 7.0, 8.0, 9.0, 10.0, 11.0);
   ASSERT_TRUE(basePose == basePoseTrue);
 
   const Eigen::Vector<double, 12> jointPositions = getJointPositions(state, info);
@@ -54,22 +54,22 @@ TEST(AccessHelperFunctions, RobotStateNonConst)
 
   // READ/WRITE ACCESS
 
-  Eigen::Vector<double, 6> baseVelocity = getBaseVelocity(state, info);
+  vector6_t baseVelocity = getBaseVelocity(state, info);
   Eigen::Vector<double, 3> baseLinearVelocity = getBaseLinearVelocity(state, info);
   Eigen::Vector<double, 3> baseAngularVelocity = getBaseAngularVelocity(state, info);
 
   ASSERT_TRUE(baseLinearVelocity== Eigen::Vector3d(0.0, 1.0, 2.0));
   ASSERT_TRUE(baseAngularVelocity == Eigen::Vector3d(3.0, 4.0, 5.0));
-  Eigen::Vector<double, 6> baseVelocityTrue = Eigen::Vector<double, 6>(0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
+  vector6_t baseVelocityTrue = vector6_t(0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
   ASSERT_TRUE(baseVelocity == baseVelocityTrue);
   
-  Eigen::Vector<double, 6> basePose = getBasePose(state, info);
+  vector6_t basePose = getBasePose(state, info);
   Eigen::Vector<double, 3> basePosition = getBasePosition(state, info);
   Eigen::Vector<double, 3> baseOrientation = getBaseOrientationZyx(state, info);
 
   ASSERT_TRUE(basePosition == Eigen::Vector3d(6.0, 7.0, 8.0));
   ASSERT_TRUE(baseOrientation == Eigen::Vector3d(9.0, 10.0, 11.0));
-  Eigen::Vector<double, 6> basePoseTrue = Eigen::Vector<double, 6>(6.0, 7.0, 8.0, 9.0, 10.0, 11.0);
+  vector6_t basePoseTrue = vector6_t(6.0, 7.0, 8.0, 9.0, 10.0, 11.0);
   ASSERT_TRUE(basePose == basePoseTrue);
 
   Eigen::Vector<double, 12> jointPositions = getJointPositions(state, info);
